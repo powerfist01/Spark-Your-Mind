d5231824c95a294a51c7c747051e9b40ce228746
powerfist01 <sujeets207@gmail.com>
2020-04-22 16:21:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var ejs = require('ejs')
+var path = require('path')
+var app = express()
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'))
+app.use('/public',express.static(__dirname + '/public'))
+
+app.get('/',function(req,res){
+    res.render('index')
+})
+
+app.listen(3000,function(err){
+    if(err)
+        console.log('Error in starting the server')
+    console.log('Server running on http://localhost:3000')
+})

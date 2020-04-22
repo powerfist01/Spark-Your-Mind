737a6fbc046077d1553311bbcc46037b5225210a
powerfist01 <sujeets207@gmail.com>
2020-04-22 23:48:24 +0530

    Added and Updated views

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'))
 app.use('/public',express.static(__dirname + '/public'))
 
+
 app.get('/',function(req,res){
     res.render('index')
+})
+app.get('/addQuestion',function(req,res){
+    res.render('addquestion')
 })
 
 app.listen(3000,function(err){
